7c6b816aa661a769e4bb6566d77f601f9d48c8b7
kc <k.netpost@gmail.com>
2022-03-18 17:07:40 +0100

    small refactoring

--- commands/dns.py
+++ commands/dns.py
@@ -8,19 +8,18 @@ from commands.basecommand import BaseCommand
 
 class DNS(BaseCommand):
     def __init__(self):
+        self.rx = re.compile('(\d+d)?(\d+h)?(\d+m)?(\d+s)?')
         self.__name__ = 'DNS Cache'
 
     def get_seconds(self, ttl):
-        rx = re.compile('(\d+d)?(\d+h)?(\d+m)?(\d+s)?')
-        groups = rx.match(ttl).groups()
+        groups = self.rx.match(ttl).groups()
         d=0 if not groups[0] else int(groups[0][:-1])
         h=0 if not groups[1] else int(groups[1][:-1])
         m=0 if not groups[2] else int(groups[2][:-1])
         s=0 if not groups[3] else int(groups[3][:-1])
         h=h+(d*24)
-        s=s+(m*60)+((h*60)*60)
-
-        return int(s)
+        s=s+(m*60)+(h*3600)
+        return s
 
     def run_ssh(self, sshc):
         data = self._ssh_data(sshc, '/ip dns print')
