b9b3d3788bed76e6d913b778bbe44e576e9440c0
Krzysztof Cieslak <k.netpost@gmail.com>
2022-03-18 14:16:09 +0100

    Fix: TTL value, invalid literal for int()

--- commands/dns.py
+++ commands/dns.py
@@ -1,12 +1,26 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-from commands.basecommand import BaseCommand
+import time
+import re
 
+from commands.basecommand import BaseCommand
 
 class DNS(BaseCommand):
     def __init__(self):
         self.__name__ = 'DNS Cache'
+
+    def get_seconds(self, ttl):
+        rx = re.compile('(\d+d)?(\d+h)?(\d+m)?(\d+s)?')
+        groups = rx.match(ttl).groups()
+        d=0 if not groups[0] else int(groups[0][:-1])
+        h=0 if not groups[1] else int(groups[1][:-1])
+        m=0 if not groups[2] else int(groups[2][:-1])
+        s=0 if not groups[3] else int(groups[3][:-1])
+        h=h+(d*24)
+        s=s+(m*60)+((h*60)*60)
+
+        return int(s)
 
     def run_ssh(self, sshc):
         data = self._ssh_data(sshc, '/ip dns print')
@@ -24,18 +38,11 @@ class DNS(BaseCommand):
         recommendation = []
 
         for item in res:
-            if int(item['ttl'].partition('s')[0]) > 200000:
-                sus_dns.append(f'Domain name: {item["name"]} with ip {item["address"]}: might be DNS poisoning- '
+            if self.get_seconds(item['ttl']) > 200000:
+                sus_dns.append(f'Domain name: {item["name"]} with ip {item["data"]}: might be DNS poisoning- '
                                f'severity: high')
 
         if enabled:
             recommendation.append('In case DNS cache is not required on your router - disable it')
 
         return sus_dns, recommendation
-
-
-
-
-
-
-
