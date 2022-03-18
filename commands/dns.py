4254ab2cb69f46c0d78680fd6eb3f486b93df14b
Vadim YR <i39@users.noreply.github.com>
2022-03-18 14:08:13 +0200

    Some changes for working with Mikrotik since 6.46

--- commands/dns.py
+++ commands/dns.py
@@ -1,8 +1,21 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-
+import re
 from commands.basecommand import BaseCommand
 
+def get_value(S, i):
+    try:
+        if S[i] == "" :
+            return 0
+        return int(S[i])
+    except IndexError:
+        return 0
+
+def to_integer(dt_time):
+    # code for parsing dt_time
+    parse_list = re.split("[w,d,h,m,s]+",dt_time)
+    print(parse_list)
+    return (get_value(parse_list,0)*7+get_value(parse_list,1))*1000000 + get_value(parse_list,2)*10000  +  get_value(parse_list,3)*100 + get_value(parse_list,4)
 
 class DNS(BaseCommand):
     def __init__(self):
@@ -12,6 +25,7 @@ class DNS(BaseCommand):
         data = self._ssh_data(sshc, '/ip dns print')
         enabled = 'allow-remote-requests: yes' in data.lower()
 
+        #res = self._ssh_data_with_header(sshc, '/ip dns cache print detail')
         res = self._ssh_data_with_header(sshc, '/ip dns cache print detail')
         sus_dns, recommendation = self.check_results_ssh(res, enabled)
 
@@ -24,8 +38,8 @@ class DNS(BaseCommand):
         recommendation = []
 
         for item in res:
-            if int(item['ttl'].partition('s')[0]) > 200000:
-                sus_dns.append(f'Domain name: {item["name"]} with ip {item["address"]}: might be DNS poisoning- '
+            if to_integer(item['ttl'].partition('s')[0]) > 200000:
+                sus_dns.append(f'Domain name: {item["name"]} with ip {item["data"]}: might be DNS poisoning- '
                                f'severity: high')
 
         if enabled:
