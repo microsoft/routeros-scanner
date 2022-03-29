54d23842526c187e497a231aa749c32d430718b5
noafru <noaf@microsoft.com>
2022-03-29 13:11:35 +0300

    merge with main

--- commands/dns.py
+++ commands/dns.py
@@ -1,5 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
+import traceback
+import re
+import sys
 
 from commands.basecommand import BaseCommand
 
@@ -23,15 +26,28 @@ class DNS(BaseCommand):
         sus_dns = []
         recommendation = []
 
-        for item in res:
-            if int(item['ttl'].partition('s')[0]) > 200000:
-                sus_dns.append(f'Domain name: {item["name"]} with ip {item["address"]}: might be DNS poisoning- '
-                               f'severity: high')
+        try:
+            for item in res:
+                if self.calc_sec(item['ttl']) > 200000:
+                    address_key = "address"
+                    if "data" in item:
+                        address_key = "data" #as seen in RouterOs 7.x
+
+                    sus_dns.append(f'Domain name: {item["name"]} with ip {item[address_key]}: might be DNS poisoning - '
+                                   f'severity: high')
+        except Exception:
+            print(traceback.format_exc(), file = sys.stderr)
 
         if enabled:
             recommendation.append('In case DNS cache is not required on your router - disable it')
 
         return sus_dns, recommendation
+
+    def calc_sec(self, ttl):
+        to_sec = {'s': 1, 'm': 60, 'h':3600, 'd':86400, 'w': 604800}
+        time = re.findall(r"(\d+)([a-z])", ttl)
+        return sum(list(map(lambda item: int(item[0]) * to_sec[item[1]], time)))
+
 
 
 
--- commands/version.py
+++ commands/version.py
@@ -1,9 +1,11 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
+import traceback
+import re
+import sys
 
 from commands.basecommand import BaseCommand
 from nvd import CVEValidator
-import re
 
 
 class Version(BaseCommand):
@@ -13,10 +15,14 @@ class Version(BaseCommand):
     def run_ssh(self, sshc):
         version = ''
         data = self._ssh_data(sshc, ':put [/system resource get version]')
-        version_reg = re.search(r'([\d\.]+)', data)
 
-        if version_reg:
-            version = version_reg.group(1)
+        try:
+            version_reg = re.search(r'([\d\.]+)', data)
+
+            if version_reg:
+                version = version_reg.group(1)
+        except Exception:
+            print(traceback.format_exc())
 
         sus, recommendation = self.check_results_ssh(version)
 
@@ -28,12 +34,15 @@ class Version(BaseCommand):
         sus_version = []
         recommendation = []
 
-        if version:
-            cve = CVEValidator('./assets/mikrotik_cpe_match.json')
-            ver_cves = cve.check_version(version)
-            if ver_cves:
-                sus_version = ver_cves
-                recommendation.append(f'RouterOS version: {version} is vulnerable to CVE(s). Upgrade to the latest version.')
+        try:
+            if version:
+                cve = CVEValidator('./assets/mikrotik_cpe_match.json')
+                ver_cves = cve.check_version(version)
+                if ver_cves:
+                    sus_version = ver_cves
+                    recommendation.append(f'RouterOS version: {version} is vulnerable to CVE(s). Upgrade to the latest version.')
+        except Exception:
+            print(traceback.format_exc(), file = sys.stderr)
 
         return sus_version, recommendation
 
