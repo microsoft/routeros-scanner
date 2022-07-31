fffa83486729ef1445b4b03a10cb6e4efbb28bb8
Orel Kayam <orkayam@microsoft.com>
2022-07-31 16:00:19 +0300

    updates

--- restore.sh
+++ restore.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cur_dir="$(dirname ${BASH_SOURCE[0]})"
-pip install -r $cur_dir/../../requirements.txt
+# exit immediately when one publish failed
+set -e
+
+ROOT_DIR="$(dirname $0)/.."
+
+pip install -r "$ROOT_DIR/requirements.txt"
